c94c500dd28070212d2e19de1e8639680d616de9
NaturelLaurent <laurent.chaloupe@gmail.com>
2020-11-16 16:02:58 +0100

    exo mvc à completé

---
+++ src/Entity/User.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class User
+{
+    private string $prenom;
+    private string $login;
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     */
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+
+
+
+}
