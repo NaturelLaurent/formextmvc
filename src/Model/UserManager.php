4c392045d2ad03f7e354497190e4c431355db11a
alexisdournin <alexisdournin@gmail.com>
2020-11-23 11:45:04 +0100

    test

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -1,21 +1,16 @@
 <?php
 
 class UserManager
-<<<<<<< HEAD
 
-=======
->>>>>>> 628394a548c960284c2b72f1e568206994d0a525
 {
     private array $info;
+""
 
     public function __construct()
     {
         $this->info = [
-<<<<<<< HEAD
             'login'=>'root',
             'nom' =>'root'
-=======
->>>>>>> 628394a548c960284c2b72f1e568206994d0a525
         ];
     }
 
