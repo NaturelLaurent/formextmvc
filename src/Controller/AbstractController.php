22da897ad2832fde992d917a7e41e6288f1aebe2
NaturelLaurent <laurent.chaloupe@gmail.com>
2020-11-24 13:32:19 +0100

    nouveau cours sur l'API REST

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+class AbstractController
+{
+    public function json(?array $json, int $status): void
+    {
+        header('Content-Type: application/json; charset=utf-8');
+
+        if ($status) {
+            http_response_code($status);
+        }
+
+        echo json_encode($json, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
+    protected function render(string $_tpl, array $param = null): void
+    {
+        define('NOM_TPL', $_tpl);
+
+        if ($param) {
+            extract($param, EXTR_SKIP);
+        }
+
+        require dirname(__DIR__) . '/../templates/' . NOM_TPL . '.php';
+    }
+}
