2f889f2b9eb4ed96ac4bff4faa42d70ce94f4aa0
NaturelLaurent <laurent.chaloupe@gmail.com>
2020-11-30 14:09:14 +0100

    cours api restful symfony

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,25 +4,63 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\DenormalizableInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user1", name="list_user", methods={"GET"})
+     * @Route("/user", name="list_user", methods={"GET"})
      */
     public function userLireMethodeUn(UserRepository $userRepository, NormalizerInterface $normalizer) : Response
     {
         $users = $userRepository->findAll();
 
-        $userArray = $normalizer->normalize($users);
+        $userArray = $normalizer->normalize($users, null, ['groups' => 'user:read']);
 
         $json = json_encode($userArray);
 
         return new Response($json, Response::HTTP_OK, ['Content-Type' => "application/json"]);
+    }
+
+    /**
+     * @Route("/user2", name="list_user2", methods={"GET"})
+     */
+    public function userLireMethodeDeux(UserRepository $userRepository, NormalizerInterface $normalizer) : Response
+    {
+        $users = $userRepository->findAll();
+
+        $userArray = $normalizer->normalize($users, null, ['groups' => 'user:read']);
+
+        return $this->json($userArray, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/user3", name="list_user3", methods={"GET"})
+     */
+    public function userLireMethodeTrois(UserRepository $userRepository) : Response
+    {
+        return $this->json($userRepository->findAll(), Response::HTTP_OK, [], ['groups' => 'user:read']);
+    }
+
+    /**
+     * @Route("user", name="user_new", methods={"POST"})
+     */
+    public function userNewMethodeUn(Request $request, SerializerInterface $serializer, EntityManagerInterface $em) : Response
+    {
+        $json = $request->getContent();
+        $user = $serializer->deserialize($json, User::class, 'json');
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json($user, Response::HTTP_OK);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -18,22 +19,26 @@ class User implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups("user:read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups("user:read")
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @Groups("user:read")
      */
     private $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups("user:read")
      */
     private $password;
 
