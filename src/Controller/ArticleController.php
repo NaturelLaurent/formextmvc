fe9a182a37376e60fc43b39554500f6c70b1e1ef
adil <raadel3imlak@outlook.fr>
2020-12-01 11:00:17 +0100

    article ok

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+
+use ApiPlatform\Core\Validator\ValidatorInterface;
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @Route("/article")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="article_index", methods={"GET"})
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->json($articleRepository->findAll(), Response::HTTP_OK);
+    }
+
+
+    /**
+     * @Route("/", name="article_new", methods={"POST"})
+     */
+    public function new(
+        Request $request,
+        SerializerInterface $serialiser,
+        ValidatorInterface $validator,
+        UserPasswordEncoderInterface $encoder
+    ): Response {
+        $json = $request->getContent();
+
+        $article = $serialiser->deserialize($json, Article::class, 'json');
+
+        $errors = $validator->validate($article);
+        if ($errors) {
+            return $this->json($errors, Response::HTTP_BAD_REQUEST);
+        }
+
+      
+        try {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+        } catch (Exception $e) {
+            return $this->json(['message' => $e->getMessage()], Response::HTTP_FORBIDDEN);
+        }
+
+        return $this->json(['message' => "Article créer"], Response::HTTP_OK);
+    }
+
+
+    /**
+     * @Route("/{id}", name="article_show", methods={"GET"})
+     */
+    public function show(Article $article,ArticleRepository $articleRepository): Response
+    {
+        try {
+            $user = $articleRepository->findBy(['id'=>$article->getId()]);
+            
+        } catch (Exception $e) {
+            return $this->json(['message' => 'Article non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+       
+            return $this->json($user, Response::HTTP_OK);
+    }
+
+  
+    /**
+     * @Route("/{id}", name="article_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        try {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+            
+        } catch (Exception $e) {
+           return $this->json(['message' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+        
+        return $this->json(['message' => "Article supprimer"], Response::HTTP_OK);         
+       
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Core\Annotation\ApiResource;
@@ -38,6 +39,18 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Article::class, mappedBy="user")
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+  
 
     public function getId(): ?int
     {
@@ -116,4 +129,36 @@ class User implements UserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->removeElement($article)) {
+            // set the owning side to null (unless already changed)
+            if ($article->getUser() === $this) {
+                $article->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+ 
 }
